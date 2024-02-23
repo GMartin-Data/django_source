7e46a6f33c718e4f2719ce691c05b53313b54dd2
GMartin-Data <gregory.martin.data@gmail.com>
2024-02-23 18:16:15 +0100

    Update paths in BASE_DIR and manage.py

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ from dotenv import read_dotenv
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.base')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
