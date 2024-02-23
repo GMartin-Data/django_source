64b128e77a9951a1bae108e8ddbb2b2a5a740dc5
GMartin-Data <gregory.martin.data@gmail.com>
2024-02-23 18:08:19 +0100

    Manage secrets

--- manage.py
+++ manage.py
@@ -3,6 +3,8 @@
 import os
 import sys
 
+from dotenv import read_dotenv
+
 
 def main():
     """Run administrative tasks."""
@@ -19,4 +21,5 @@ def main():
 
 
 if __name__ == '__main__':
+    read_dotenv()
     main()
--- project/settings.py
+++ project/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fmodm43_#bz8=9ausnx4%dxs%tc(oe27f$rh=_t$7zu_en%qir'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.getenv('DEBUG') == '1')
 
 ALLOWED_HOSTS = []
 
