27869ce0b2f4747d708d582e4708482af85dbac2
GMartin-Data <gregory.martin.data@gmail.com>
2024-02-24 16:27:15 +0100

    Add logic on navbar to access 'contact' and 'about' pages

--- main/views.py
+++ main/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/home.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
