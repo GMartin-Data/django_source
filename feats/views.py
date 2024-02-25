332167ff561231bbe3eefd53af600d97b2b3fd1f
GMartin-Data <gregory.martin.data@gmail.com>
2024-02-25 17:50:51 +0100

    Implement logic to require login to access a page via classes

--- feats/views.py
+++ feats/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView, ListView
 
 from .models import Feature
 
 
-class FeaturesListView(ListView):
+# Note: LoginRequiredMixin has to be on the left
+# In order to be the first class to inherit from
+class FeaturesListView(LoginRequiredMixin, ListView):
     model = Feature
     template_name = "feats/feats_list.html"
 
