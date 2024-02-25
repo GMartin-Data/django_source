2b13857e26a36c08519f442176e81415d26a4fe3
GMartin-Data <gregory.martin.data@gmail.com>
2024-02-25 16:45:32 +0100

    Add accounts/ url to project/urls.py to allow authentication

--- project/urls.py
+++ project/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('feats/', include('feats.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
