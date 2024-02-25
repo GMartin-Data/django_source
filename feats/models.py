d3434eba022a0f6f33e61f492a3873a5dfd46e3a
GMartin-Data <gregory.martin.data@gmail.com>
2024-02-25 11:38:27 +0100

    Add 'Feature' model in 'feats' app and register it

--- feats/models.py
+++ feats/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Feature(models.Model):
+    name = models.CharField(
+        max_length = 100,
+        null = False, blank = False  # These are default values btw
+    )
+    description = models.TextField(
+        max_length = 500,
+        null = False, blank = False
+    )
+    
